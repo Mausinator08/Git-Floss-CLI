e9f3a73536c75dba8affd2c5f40b5d88d0713c3d
John Patrick Maus <mauscomp@live.com>
2021-05-10 17:59:18 -0500

    create-feature branch complete

---
+++ scripts/merge-feature.js
@@ -0,0 +1,84 @@
+"use strict";
+
+/** @module scripts/merge-feature */
+
+//#region Required
+const os = require("os");
+const { exec } = require("child_process");
+//#endregion
+
+module.exports = async (name) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      switch (os.platform()) {
+        case "win32":
+          exec(
+            `git checkout develop &&` +
+            `git merge --no-ff ${name} &&` +
+            `git branch -d ${name} &&` +
+            `git push origin develop`,
+            (error, stdout, stderr) => {
+              if (error || stderr) {
+                if (error) {
+                  reject(new Error(
+                    `The command ${error.cmd} threw an error!\nThe error code was ${error.code}.\nMessage: ${error.message}\n\n` +
+                    `output:\n${stdout}\n\n${stderr}`
+                  ));
+                  return;
+                }
+              }
+              
+              resolve(`${stdout}\nFeature ${name} created!`);
+            }
+          );
+          break;
+
+        case "darwin":
+          exec(
+            `git checkout develop &&` +
+            `git merge --no-ff ${name} &&` +
+            `git branch -d ${name} &&` +
+            `git push origin develop`,
+            (error, stdout, stderr) => {
+              if (error || stderr) {
+                if (error) {
+                  reject(new Error(
+                    `The command ${error.cmd} threw an error!\nThe error code was ${error.code}.\nMessage: ${error.message}\n\n` +
+                    `output:\n${stdout}\n\n${stderr}`
+                  ));
+                  return;
+                }
+              }
+              
+              resolve(`${stdout}\nFeature ${name} created!`);
+            }
+          );
+          break;
+
+        case "linux":
+          exec(
+            `git checkout develop &&` +
+            `git merge --no-ff ${name} &&` +
+            `git branch -d ${name} &&` +
+            `git push origin develop`,
+            (error, stdout, stderr) => {
+              if (error || stderr) {
+                if (error) {
+                  reject(new Error(
+                    `The command ${error.cmd} threw an error!\nThe error code was ${error.code}.\nMessage: ${error.message}\n\n` +
+                    `output:\n${stdout}\n\n${stderr}`
+                  ));
+                  return;
+                }
+              }
+              
+              resolve(`${stdout}\nFeature ${name} created!`);
+            }
+          );
+          break;
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
