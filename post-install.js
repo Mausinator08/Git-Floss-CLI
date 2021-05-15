675beb60cf16064e50e8919761ea10476a2c8c8c
John Patrick Maus <mauscomp@live.com>
2021-05-15 14:42:56 -0500

    merging feature fix-post-install

--- post-install.js
+++ post-install.js
@@ -4,15 +4,21 @@ const os = require("os");
 const { exec } = require("child_process");
 
 switch (os.platform()) {
-    case "win32":
-        exec("pkg . --output=%CD%\\.bin\\git-flow.exe && setx PATH \"%PATH%;%CD%\\.bin\"");
-        break;
-    
-    case "darwin":
-        exec("pkg . --output=$PWD/.bin/git-flow && export PATH=$PATH;$PWD'/.bin'");
-        break;
-    
-    case "linux":
-        exec("pkg . --output=$PWD/.bin/git-flow && export PATH=$PATH;$PWD'/.bin'");
-        break;
+  case "win32":
+    exec(
+      `pkg . --output=${__dirname}\\.bin\\git-flow.exe && setx PATH \"%PATH%;${__dirname}\\.bin\\\"`,
+    );
+    break;
+
+  case "darwin":
+    exec(
+      `pkg . --output=${__dirname}/.bin/git-flow && export PATH=$PATH;${__dirname}'/.bin/'`,
+    );
+    break;
+
+  case "linux":
+    exec(
+      `pkg . --output=${__dirname}/.bin/git-flow && export PATH=$PATH;${__dirname}'/.bin/'`,
+    );
+    break;
 }
