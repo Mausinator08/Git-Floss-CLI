9d3d297816d1aea3f2692addb2e80bc1e7638869
John Patrick Maus <mauscomp@live.com>
2021-05-15 23:25:34 -0500

    merging feature fix-post-install

--- post-install.js
+++ post-install.js
@@ -8,13 +8,13 @@ const { stdin } = require("process");
 switch (os.platform()) {
   case "win32":
     exec(
-      `pkg . --output=${__dirname}\\.bin\\git-flow.exe && setx PATH \"%PATH%;${__dirname}\\.bin\\\"`,
+      `pkg . --output=${__dirname}\\.bin\\git-flow.exe && setx PATH \"%PATH%;%NVM_SYMLINK%\\node_modules\\git-flow\\.bin\\\" && set PATH=\"%PATH%;%NVM_SYMLINK%\\node_modules\\git-flow\\.bin\\\"`,
       (error, stdout, stderr) => {
         if (error || stderr) {
           if (error) {
             console.error(
-                `The command ${error.cmd} threw an error!\nThe error code was ${error.code}.\nMessage: ${error.message}\n\n` +
-                  `output:\n${stdout}\n\n${stderr}`
+              `The command ${error.cmd} threw an error!\nThe error code was ${error.code}.\nMessage: ${error.message}\n\n` +
+                `output:\n${stdout}\n\n${stderr}`,
             );
             return;
           }
