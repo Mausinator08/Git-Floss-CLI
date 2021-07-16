248db8bd0ceeb01344953c111b61ae0cb55f1d54
John Patrick Maus <mauscomp@live.com>
2021-07-16 02:12:09 -0500

    VERSION gets added in the init-default-config.js script.

--- scripts/init-default-config.js
+++ scripts/init-default-config.js
@@ -1,58 +1,91 @@
-'use strict';
+"use strict";
 
 /** @module scripts/create-feature */
 
 //#region Required
-const os = require('os');
-const fs = require('fs');
+const os = require("os");
+const fs = require("fs");
 //#endregion
 
 module.exports = async (nodejs = true) => {
 	return new Promise(async (resolve, reject) => {
 		try {
 			switch (os.platform()) {
-				case 'win32':
-                    {
-						var defaultConfig = fs.readFileSync(__dirname + "..\\git-floss.json", { encoding: "utf-8" });
+				case "win32":
+					{
+						var defaultConfig = fs.readFileSync(
+							__dirname + "..\\git-floss.json",
+							{ encoding: "utf-8" }
+						);
 						defaultConfig = JSON.parse(defaultConfig);
 
 						defaultConfig.isNode = nodejs;
 
 						defaultConfig = JSON.stringify(defaultConfig, null, 4);
 
-						fs.writeFileSync(process.cwd() + "\\git-floss.json", defaultConfig, { encoding: "utf-8" });
+						fs.writeFileSync(
+							process.cwd() + "\\git-floss.json",
+							defaultConfig,
+							{ encoding: "utf-8" }
+						);
+
+						fs.writeFileSync(process.cwd() + "\\VERSION", "0.0.1", {
+							encoding: "utf-8",
+						});
 
 						resolve(`${stdout}\nDone!`);
 					}
 					break;
 
-				case 'darwin':
+				case "darwin":
 					{
-						var defaultConfig = fs.readFileSync(__dirname + "../git-floss.json", { encoding: "utf-8" });
+						var defaultConfig = fs.readFileSync(
+							__dirname + "../git-floss.json",
+							{ encoding: "utf-8" }
+						);
 						defaultConfig = JSON.parse(defaultConfig);
 
 						defaultConfig.isNode = nodejs;
 
 						defaultConfig = JSON.stringify(defaultConfig, null, 4);
 
-						fs.writeFileSync(process.cwd() + "/git-floss.json", defaultConfig, { encoding: "utf-8" });
+						fs.writeFileSync(
+							process.cwd() + "/git-floss.json",
+							defaultConfig,
+							{ encoding: "utf-8" }
+						);
+
+						fs.writeFileSync(process.cwd() + "\\VERSION", "0.0.1", {
+							encoding: "utf-8",
+						});
 
 						resolve(`${stdout}\nDone!`);
 					}
 					break;
 
-				case 'linux':
+				case "linux":
 					{
-						var defaultConfig = fs.readFileSync(__dirname + "../git-floss.json", { encoding: "utf-8" });
+						var defaultConfig = fs.readFileSync(
+							__dirname + "../git-floss.json",
+							{ encoding: "utf-8" }
+						);
 						defaultConfig = JSON.parse(defaultConfig);
 
 						defaultConfig.isNode = nodejs;
 
 						defaultConfig = JSON.stringify(defaultConfig, null, 4);
 
-						fs.writeFileSync(process.cwd() + "/git-floss.json", defaultConfig, { encoding: "utf-8" });
+						fs.writeFileSync(
+							process.cwd() + "/git-floss.json",
+							defaultConfig,
+							{ encoding: "utf-8" }
+						);
 
-                        resolve(`${stdout}\nDone!`);
+						fs.writeFileSync(process.cwd() + "\\VERSION", "0.0.1", {
+							encoding: "utf-8",
+						});
+
+						resolve(`${stdout}\nDone!`);
 					}
 					break;
 			}
